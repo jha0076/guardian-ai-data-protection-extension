d0470943a6c2b0b8fae8cb775b687bd7520b5d81
Kumar Satyam <71371931+jha0076@users.noreply.github.com>
2025-05-06 00:04:00 +0530

    Delete data-guardian-dev directory

--- data-guardian-dev/Frontend/contentScript2.js
+++
@@ -1,9 +0,0 @@
-  
-if (window.location.href.startsWith('https://chat.openai.com')){
-    let textAreaElement = document.getElementById('prompt-textarea');
-    chrome.storage.local.get('protectedText', function(data) {
-
-            textAreaElement.value = data.protectedText || '';
-    
-      });
-}
--- data-guardian-dev/Frontend/popup.js
+++
@@ -1,9 +0,0 @@
-// popup.js
-document.addEventListener('DOMContentLoaded', function() {
-    chrome.storage.local.get('pastedText', function(data) {
-      const pastedTextarea = document.getElementById('prompt-textarea');
-      if (pastedTextarea) {
-        pastedTextarea.value = data.pastedText || '';
-      }
-    });
-  });
